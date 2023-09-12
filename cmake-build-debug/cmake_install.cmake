947f9ca0ea66bbb0a1accc10b4c80fb4006223a4
Choppy <kaedongeddes@sd28.org>
2023-09-12 14:44:27 -0700

    beginning trying to set things up
    couldn't figure out stoi crash handling

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/kaedo/OneDrive/Documents/GitHub/Rock-Paper-Scissors-Again
+# Install script for directory: C:/Users/chop/Documents/GitHub/Rock-Paper-Scissors-Again
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/kaedo/OneDrive/Documents/GitHub/Rock-Paper-Scissors-Again/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/chop/Documents/GitHub/Rock-Paper-Scissors-Again/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
