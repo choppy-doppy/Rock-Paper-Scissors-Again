2755e3b7cdb2b6dc71638df0edfb8c61d1480e5a
Choppy <kaedongeddes@sd28.org>
2023-09-14 11:25:02 -0700

    bug fix
    ++ Fixed a bug where if you played rock and it played scissors it would say it won
    
    -- Arguably made the code look worse

--- main.cpp
+++ main.cpp
@@ -9,44 +9,42 @@ void randomizer();
 
 int main() {
     cout << "Type rock, paper, or scissors!" << endl;
-    // sets the current time as a random seed to make the random generation a little more random
-    // jumps to the function that checks for exit codes and invalid responses
+    srand(GetTickCount());
     randomizer();
 }
 
 void randomizer() {
     for ( ; ; ) {
         string userInput;
-        // variable that contains the 3 valid responses
         vector<string> choice = {"rock", "paper", "scissors"};
-        // checks for user input
         cin >> userInput;
-        // checks for exit command and valid responses
         if (userInput == "exit")
             exit(2);
         if (userInput == choice[0] || userInput == choice[1] || userInput == choice[2]) {
-
             vector<string> options = {"rock", "paper", "scissors"};
+            int randIndex = rand() % 3;
 
-            random_device rd;
-            mt19937 mt(rd());
-            uniform_int_distribution<int> dist(0, 2);
-
-            // converts the integer from the randIndex into a string
-            if (dist == 0) {
+            if (randIndex == 0)
                 cout << "   rock" << endl;
-            } else if (dist == 1) {
+            else if (randIndex == 1)
                 cout << "   paper" << endl;
-            } else if (dist == 2) {
+            else
                 cout << "   scissors" << endl;
-            }
 
-            // checking win conditions
-            if (userInput == choice[0] && dist == 1 || userInput == choice[1] && dist == 2 || userInput == choice[2] && dist == 1) {
-                cout << "   i win!!" << endl;
-            } else if (userInput == choice[0] && dist == 2 || userInput == choice[1] && dist == 0 || userInput == choice[2] && dist == 1)
-                cout << "   aww i lose :(" << endl;
+            if (userInput == choice[0] && randIndex == 1)
+                cout << "   I win :)" << endl;
+            else if (userInput == choice[1] && randIndex == 2)
+                cout << "   I win :)" << endl;
+            else if (userInput == choice[2] && randIndex == 1)
+                cout << "   I win :)" << endl;
 
+            if (userInput == choice[0] && randIndex == 2)
+                cout << "   I lose :(" << endl;
+            else if (userInput == choice[1] && randIndex == 0)
+                cout << "   I lose :(" << endl;
+            else if (userInput == choice[2] && randIndex == 1)
+                cout << "   I lose" << endl;
+            
         } else
             cout << "INVALID RESPONSE" << endl;
     }
